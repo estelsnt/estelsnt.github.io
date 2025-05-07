f28a507b78df5af058dda46332af700b2db3115a
estelsnt <peraltajohnestel@gmail.com>
2025-05-07 15:10:43 +0800

    prepared routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import Routing from "./pages/Routing";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
 
   return (
-    <>
-      <Routing />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
